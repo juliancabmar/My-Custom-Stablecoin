2444b23cb34ae33670ea89e2c23cce1de09e2839
julian cabrera marceglia <juliancabmar@gmail.com>
2025-03-20 08:45:41 -0300

    sorting the code

--- src/JSCEngine.sol
+++ src/JSCEngine.sol
@@ -6,9 +6,10 @@ pragma solidity ^0.8.20;
 
 // Import statements
 import {JuliansStableCoin} from "src/JuliansStableCoin.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ReentrancyGuard} from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 // import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; NOT WORK REMAPING... WHY?
 
 // Events (global)
@@ -16,10 +17,6 @@ import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/s
 // Interfaces
 // Libraries
 // Contracts
-
-
-
-
 
 /**
  * @title JSCEngine
@@ -37,18 +34,9 @@ import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/s
  * the use of the "penalty" for the liquidation process.
  */
 
-//// Imports
-
-import {JuliansStableCoin} from "src/JuliansStableCoin.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; NOT WORK REMAPING... WHY?
-
 contract JSCEngine is ReentrancyGuard {
-// INSIDE CONTRACTS,LIBRARIES or INTERFACES
-// Type declarations
-// State variables (#1:Constants, #2:Immutables, #3:Storage)
+    // Type declarations
+    // State variables (#1:Constants, #2:Immutables, #3:Storage)
     uint256 constant LIQUIDATION_THRESHOLD = 50; // Can only mint JSC for 50% of the collateral value.
     uint256 constant LIQUIDATION_PRECISION = 100;
     uint256 constant PRECISION = 1e18;
@@ -57,14 +45,19 @@ contract JSCEngine is ReentrancyGuard {
     JuliansStableCoin private immutable i_jsc;
 
     mapping(address token => address priceFeed) private s_priceFeeds;
-    mapping(address user => mapping(address tokenAddress => uint256 amount)) private s_collateralDeposited;
+    mapping(address user => mapping(address tokenAddress => uint256 amount))
+        private s_collateralDeposited;
     mapping(address user => uint256 amountOfJscMinted) private s_jscMinted;
     address[] private s_colateralTokens;
 
-// Events
-    event CollateralDeposited(address indexed user, address indexed tokenAddress, uint256 indexed amount);
+    // Events
+    event CollateralDeposited(
+        address indexed user,
+        address indexed tokenAddress,
+        uint256 indexed amount
+    );
 
-// Errors (local)
+    // Errors (local)
     error JSCEngine__NeedsMoreThanZero();
     error JSCEngine__TokenAddressesAndPriceFeedsAddressesMustBeSameLength();
     error JSCEngine__InvalidTokenAddress();
@@ -72,7 +65,7 @@ contract JSCEngine is ReentrancyGuard {
     error JSCEngine__BrokenHealthFactor(address user, uint256 healthFactor);
     error JSCEngine__MintFailed();
 
-// Modifiers
+    // Modifiers
     modifier moreThanZero(uint256 _amount) {
         if (_amount <= 0) {
             revert JSCEngine__NeedsMoreThanZero();
@@ -87,8 +80,12 @@ contract JSCEngine is ReentrancyGuard {
         _;
     }
 
-// constructor
-    constructor(address[] memory _tokenAddresses, address[] memory _priceFeedsAddresses, address _jscAddress) {
+    // constructor
+    constructor(
+        address[] memory _tokenAddresses,
+        address[] memory _priceFeedsAddresses,
+        address _jscAddress
+    ) {
         if (_tokenAddresses.length != _priceFeedsAddresses.length) {
             revert JSCEngine__TokenAddressesAndPriceFeedsAddressesMustBeSameLength();
         }
@@ -99,28 +96,18 @@ contract JSCEngine is ReentrancyGuard {
         i_jsc = JuliansStableCoin(_jscAddress);
     }
 
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// view & pure functions
-
-
+    // receive function (if exists)
+    // fallback function (if exists)
+    // external
     function depositCollateralAndMintJsc() external {}
 
     function redeemCollateralFromJsc() external {}
 
-    function burnJsc() external {}
-
     function liquidate() external {}
 
-    function getHealthFactor() external view {
-
-    } // get the "collateralization ratio" in MakerDAO
-
-    function mintJsc(uint256 _amountJscToMint) external moreThanZero(_amountJscToMint) {
+    function mintJsc(
+        uint256 _amountJscToMint
+    ) external moreThanZero(_amountJscToMint) {
         s_jscMinted[msg.sender] += _amountJscToMint;
         _revertIfHealthFactorIsBroken(msg.sender);
         bool minted = i_jsc.mint(msg.sender, _amountJscToMint);
@@ -129,11 +116,59 @@ contract JSCEngine is ReentrancyGuard {
         }
     }
 
-    function _userHealthFactor(address _user) private view returns (uint256 healthFactorPercentage) {
-        (uint256 totalJscMinted, uint256 totalCollateralValueInUsd) = _getAccountInformation(_user);
-        uint256 jscMintLimit = (totalCollateralValueInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
-        return (jscMintLimit * PRECISION) / totalJscMinted;
+    // public
+
+    function depositCollateral(
+        address _tokenForCollateralAddress,
+        uint256 _amountOfCollateral
+    )
+        public
+        moreThanZero(_amountOfCollateral)
+        isAllowedToken(_tokenForCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][
+            _tokenForCollateralAddress
+        ] += _amountOfCollateral;
+        emit CollateralDeposited(
+            msg.sender,
+            _tokenForCollateralAddress,
+            _amountOfCollateral
+        );
+        bool success = ERC20(_tokenForCollateralAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _amountOfCollateral
+        );
+        if (!success) {
+            revert JSCEngine__TransferFailed();
+        }
     }
+
+    function _getCollateralValue(
+        address _user
+    ) public view returns (uint256 totalCollateralValueInUsd) {
+        for (uint256 i = 0; i < s_colateralTokens.length; i++) {
+            address token = s_colateralTokens[i];
+            uint256 amount = s_collateralDeposited[_user][token];
+            totalCollateralValueInUsd += getUsdValue(token, amount);
+        }
+    }
+
+    function getUsdValue(
+        address _token,
+        uint256 _amount
+    ) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            s_priceFeeds[_token]
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        uint8 usdDecimals = priceFeed.decimals();
+        // The price had 8 extra decimals
+        return (uint256(price) * _amount) / (10 ** usdDecimals);
+    }
+
+    // internal
 
     function _revertIfHealthFactorIsBroken(address _user) internal view {
         uint256 userHealthFactor = _userHealthFactor(_user);
@@ -142,7 +177,17 @@ contract JSCEngine is ReentrancyGuard {
         }
     }
 
-    function _getAccountInformation(address _user)
+    function revertIfHealthFactorIsBroken(address _user) internal view {}
+
+    // private
+
+    function _redeemCollateral() private {}
+
+    function _burnJsc() private {}
+
+    function _getAccountInformation(
+        address _user
+    )
         private
         view
         returns (uint256 totalJscMinted, uint256 totalCollateralValueInUsd)
@@ -151,46 +196,15 @@ contract JSCEngine is ReentrancyGuard {
         totalCollateralValueInUsd = _getCollateralValue(_user);
     }
 
-    function _getCollateralValue(address _user) public view returns (uint256 totalCollateralValueInUsd) {
-        for (uint256 i = 0; i < s_colateralTokens.length; i++) {
-            address token = s_colateralTokens[i];
-            uint256 amount = s_collateralDeposited[_user][token];
-            totalCollateralValueInUsd += getUsdValue(token, amount);
-        }
-    }
-
-    function getUsdValue(address _token, uint256 _amount) public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[_token]);
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        uint8 usdDecimals = priceFeed.decimals();
-        // The price had 8 extra decimals 
-        return (uint256(price) * _amount) / (10**usdDecimals);
-    }
-
-    function revertIfHealthFactorIsBroken(address _user) internal view {}
-
-    function redeemCollateral() internal {}
-
-    function test() external pure returns (string memory) {
-        return "HOLA";
-    }
-
-    /**
-     *
-     * @param _tokenForCollateralAddress: the address of the token to deposit as collateral
-     * @param _amountOfCollateral: the amount of tokens depositated as collateral
-     */
-    function depositCollateral(address _tokenForCollateralAddress, uint256 _amountOfCollateral)
-        internal
-        moreThanZero(_amountOfCollateral)
-        isAllowedToken(_tokenForCollateralAddress)
-        nonReentrant
-    {
-        s_collateralDeposited[msg.sender][_tokenForCollateralAddress] += _amountOfCollateral;
-        emit CollateralDeposited(msg.sender, _tokenForCollateralAddress, _amountOfCollateral);
-        bool success = ERC20(_tokenForCollateralAddress).transferFrom(msg.sender, address(this), _amountOfCollateral);
-        if (!success) {
-            revert JSCEngine__TransferFailed();
-        }
+    function _userHealthFactor(
+        address _user
+    ) private view returns (uint256 healthFactorPercentage) {
+        (
+            uint256 totalJscMinted,
+            uint256 totalCollateralValueInUsd
+        ) = _getAccountInformation(_user);
+        uint256 jscMintLimit = (totalCollateralValueInUsd *
+            LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
+        return (jscMintLimit * PRECISION) / totalJscMinted;
     }
 }
